9a01ff72d3f0ec3b785e43d09967122ddbe4c47c
heresalexandria <70348962+heresalexandria@users.noreply.github.com>
2026-08-19 17:38:07 -0400

    Fix right-click paste on web desktop

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'app/clawnsole_app.dart';
+import 'app/text_context_menu.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await configureTextContextMenus();
   runApp(const ClawnsoleApp());
 }
